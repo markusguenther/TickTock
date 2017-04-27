2f4343c6e7cb4eecec8b8c8c42eb1be23e7348b7
Markus Guenther <markus@sgalinski.de>
2017-04-27 14:30:24 +0200

    [FEATURE] Implement start, stop and reset function

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,34 +1,146 @@
 // @flow
 
-import React, { Component } from 'react'
-import { View, Text, TouchableHighlight } from 'react-native'
-import styles from './Styles/AppStyle'
+import React, { Component } from "react";
+import { View, ScrollView, Text, TouchableHighlight } from "react-native";
+import formatTime from "minutes-seconds-milliseconds";
+import styles from "./Styles/AppStyle";
 
 /**
  * Root component to wire up ios and android.
  * Write code just ones ;)
  */
 class App extends Component {
-  render () {
+  constructor() {
+    super();
+    this._handleStartPress = this._handleStartPress.bind(this);
+    this._handleLapPress = this._handleLapPress.bind(this);
+    this.state = {
+      startTime: null,
+      timeElapsed: 0,
+      running: false,
+      laps: []
+    };
+  }
+
+  _renderStartStopButton() {
+    let style = this.state.running ? styles.stopButton : styles.startButton;
+
+    return (
+      <TouchableHighlight
+        style={[styles.button, style]}
+        onPress={this._handleStartPress}
+        underlayColor="lightgrey"
+      >
+        <Text
+          style={[
+            styles.buttonText,
+            this.state.running ? styles.stopButtonText : styles.startButtonText
+          ]}
+        >
+          {this.state.running ? "Stop" : "Start"}
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+
+  _renderLapButton() {
+    let style = this.state.running ? styles.lapButton : styles.resetButton;
+
+    return (
+      <TouchableHighlight
+        style={[styles.button, style]}
+        onPress={this._handleLapPress}
+        underlayColor="lightgrey"
+      >
+        <Text style={styles.buttonText}>
+          {this.state.running ? "Runde" : "Reset"}
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+
+  _renderLaps() {
+    return this.state.laps.map(function(time, index) {
+      const lap = index + 1;
+
+      return (
+        <View style={styles.laps} key={"lap-" + lap}>
+          <Text style={[styles.lapText, styles.lapName]}>
+            Runde: {index + 1}
+          </Text>
+          <Text style={[styles.lapText, styles.lapTime]}>
+            {formatTime(time)}
+          </Text>
+        </View>
+      );
+    });
+  }
+
+  _handleStartPress() {
+    if (this.state.running) {
+      clearInterval(this.interval);
+      this.setState({ running: false });
+      return;
+    }
+
+    this.setState({ startTime: new Date() });
+
+    this.interval = setInterval(() => {
+      this.setState({
+        timeElapsed: new Date() - this.state.startTime,
+        running: true
+      });
+    }, 30);
+  }
+
+  _handleLapPress() {
+    if (!this.state.running) {
+      this.setState({
+        startTime: null,
+        timeElapsed: 0,
+        running: false,
+        laps: []
+      });
+      return;
+    }
+
+    let lap = this.state.timeElapsed;
+    this.setState({
+      startTime: new Date(),
+      laps: this.state.laps.concat([lap])
+    });
+  }
+
+  render() {
     return (
       <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>
+            TickTock - Fynn Döring
+          </Text>
+        </View>
         <View style={styles.timeContainer}>
-          <Text style={styles.timer}>00:00:00</Text>
+          <Text style={styles.timer}>
+            {formatTime(this.state.timeElapsed)}
+          </Text>
         </View>
         <View style={styles.actionContainer}>
-          <View style={[styles.button, styles.startButton]}>
-            <Text>Start</Text>
-          </View>
-          <View style={[styles.button, styles.stopButton]}>
-            <Text>Stop</Text>
-          </View>
+          {this._renderStartStopButton()}
+          {this._renderLapButton()}
         </View>
         <View style={styles.lapsContainer}>
-          <Text>Laps</Text>
+          <ScrollView
+            ref={ref => this.scrollView = ref}
+            onContentSizeChange={(contentWidth, contentHeight) => {
+              this.scrollView.scrollToEnd();
+            }}
+          >
+            {this._renderLaps()}
+          </ScrollView>
         </View>
       </View>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- App/Containers/Styles/AppStyle.js
+++ App/Containers/Styles/AppStyle.js
@@ -1,40 +1,88 @@
-import {StyleSheet} from 'react-native'
+import { StyleSheet, Platform, NativeModules } from "react-native";
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightgrey'
+    alignItems: "stretch",
+    backgroundColor: "rgba(255,255,255,1)"
+  },
+  header: {
+    paddingTop: Platform.OS === "ios" ? 20 : NativeModules.statusBarHeight,
+    height: Platform.OS === "ios" ? 64 : 54,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1d45bb"
   },
   timeContainer: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center"
   },
   timer: {
-    fontSize: 60
+    fontSize: 70,
+    fontFamily: "Helvetica",
+    color: "rgba(0,0,0,0.6)"
   },
   actionContainer: {
     flex: 2,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "rgba(238,240,245,1)",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "rgba(111,111,111,0.4)"
+  },
+  lapsContainer: {
+    flex: 3,
+    backgroundColor: "rgba(238,240,245,1)"
+  },
+  laps: {
+    borderBottomWidth: 1,
+    borderColor: "rgba(111,111,111,0.4)",
+    justifyContent: "space-around",
+    flexDirection: "row",
+    marginHorizontal: 20,
+    paddingVertical: 10
   },
   button: {
     borderWidth: 2,
     height: 100,
     width: 100,
     borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(255,255,255,1)"
   },
   startButton: {
-    borderColor: 'green'
+    borderColor: "green"
   },
   stopButton: {
-    borderColor: 'red'
+    borderColor: "red"
   },
-  lapsContainer: {
-    flex: 4,
-    backgroundColor: 'blue'
+  buttonText: {
+    fontSize: 18
+  },
+  startButtonText: {
+    color: "green"
+  },
+  stopButtonText: {
+    color: "red"
+  },
+  headerText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  lapText: {
+    fontSize: 16
+  },
+  lapTime: {
+    color: "rgba(0,0,0,0.8)"
+  },
+  lapName: {
+    color: "rgba(0,0,0,0.4)"
   }
-})
+});
